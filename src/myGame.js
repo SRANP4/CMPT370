dbfba74f6c13b64e29caaad3d8e423a8621cc085
Samuel Whiteley <sam@cosmicbagel.com>
2020-11-24 21:27:30 -0700

    Playing around with control configuration

--- src/myGame.js
+++ src/myGame.js
@@ -18,6 +18,8 @@ const keysDownLast = new Map()
 const keysDown = new Map()
 /** @type {Map<string, boolean>} */
 const keysPressed = new Map()
+/** @type {Map<string, boolean>} */
+const keysReleased = new Map()
 
 /**
  *
@@ -34,11 +36,11 @@ export function startGame (state) {
     false
   )
 
-  document.addEventListener('keydown', event => {
+  window.addEventListener('keydown', event => {
     keysDown.set(event.key, true)
   })
 
-  document.addEventListener('keyup', event => {
+  window.addEventListener('keyup', event => {
     keysDown.set(event.key, false)
   })
 
@@ -55,20 +57,56 @@ export function fixedUpdate (state, deltaTime) {
   // handle physics here
   // Here we can add game logic, like getting player objects, and moving them, detecting collisions, you name it. Examples of functions can be found in sceneFunctions
 
-  if (keysPressed['`']) {
+  updateFlyCam(state)
+}
+
+/**
+ *
+ * @param {import('./types.js').AppState} state
+ */
+function updateFlyCam (state) {
+  if (keysPressed.get('`')) {
     flyCamEnabled = !flyCamEnabled
     console.log('fly cam: ' + flyCamEnabled)
   }
 
   if (flyCamEnabled) {
+    const moveSpeed = 0.05
+
     if (keysDown.get('a')) {
-      rotateCameraAroundYAxis(state.camera, -0.16)
-      // state.camera.position[0] += 0.05
+      // rotateCameraAroundYAxis(state.camera, -0.16)
+      state.camera.position[0] += moveSpeed
+      state.camera.center[0] += moveSpeed
     }
 
     if (keysDown.get('d')) {
-      rotateCameraAroundYAxis(state.camera, 0.16)
-      // state.camera.position[0] -= 0.05
+      // rotateCameraAroundYAxis(state.camera, 0.16)
+      state.camera.position[0] -= moveSpeed
+      state.camera.center[0] -= moveSpeed
+    }
+
+    if (keysDown.get('w')) {
+      // rotateCameraAroundYAxis(state.camera, -0.16)
+      state.camera.position[2] += moveSpeed
+      state.camera.center[2] += moveSpeed
+    }
+
+    if (keysDown.get('s')) {
+      // rotateCameraAroundYAxis(state.camera, 0.16)
+      state.camera.position[2] -= moveSpeed
+      state.camera.center[2] -= moveSpeed
+    }
+
+    if (keysDown.get(' ')) {
+      // rotateCameraAroundYAxis(state.camera, -0.16)
+      state.camera.position[1] += moveSpeed
+      state.camera.center[1] += moveSpeed
+    }
+
+    if (keysDown.get('Shift')) {
+      // rotateCameraAroundYAxis(state.camera, 0.16)
+      state.camera.position[1] -= moveSpeed
+      state.camera.center[1] -= moveSpeed
     }
   }
 }
@@ -85,11 +123,20 @@ export function update (state) {}
 function updatePressedKeys () {
   keysDown.forEach((_, key) => {
     if (keysDown.get(key) && !keysDownLast.get(key)) {
-      keysPressed[key] = true
+      console.log(key)
+      keysPressed.set(key, true)
     }
 
     if (keysDown.get(key) && keysDownLast.get(key)) {
-      keysPressed[key] = false
+      keysPressed.set(key, false)
+    }
+
+    if (!keysDown.get(key) && keysDownLast.get(key)) {
+      keysReleased.set(key, true)
+    }
+
+    if (!keysDown.get(key) && !keysDownLast.get(key)) {
+      keysReleased.set(key, false)
     }
 
     keysDownLast.set(key, keysDown.get(key))
